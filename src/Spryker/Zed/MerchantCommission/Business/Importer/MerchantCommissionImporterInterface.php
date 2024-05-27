9217d54fceb7df13c75d10c73f4413eeb36c0f45
Anton Khabiuk <anton.khabiuk@spryker.com>
2024-05-27 16:08:39 +0300

    CC-33275 Commissions management GUI. (#10946)
    CC-33275 Commissions management GUI.

--- src/Spryker/Zed/MerchantCommission/Business/Importer/MerchantCommissionImporterInterface.php
+++ src/Spryker/Zed/MerchantCommission/Business/Importer/MerchantCommissionImporterInterface.php
@@ -5,27 +5,19 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\MerchantCommission\Business\Updater;
+namespace Spryker\Zed\MerchantCommission\Business\Importer;
 
-use ArrayObject;
 use Generated\Shared\Transfer\MerchantCommissionCollectionRequestTransfer;
 use Generated\Shared\Transfer\MerchantCommissionCollectionResponseTransfer;
 
-interface MerchantCommissionUpdaterInterface
+interface MerchantCommissionImporterInterface
 {
     /**
      * @param \Generated\Shared\Transfer\MerchantCommissionCollectionRequestTransfer $merchantCommissionCollectionRequestTransfer
      *
      * @return \Generated\Shared\Transfer\MerchantCommissionCollectionResponseTransfer
      */
-    public function updateMerchantCommissionCollection(
+    public function importMerchantCommissionCollection(
         MerchantCommissionCollectionRequestTransfer $merchantCommissionCollectionRequestTransfer
     ): MerchantCommissionCollectionResponseTransfer;
-
-    /**
-     * @param \ArrayObject<array-key, \Generated\Shared\Transfer\MerchantCommissionTransfer> $merchantCommissionTransfers
-     *
-     * @return \ArrayObject<array-key, \Generated\Shared\Transfer\MerchantCommissionTransfer>
-     */
-    public function updatePreValidatedMerchantCommissions(ArrayObject $merchantCommissionTransfers): ArrayObject;
 }
